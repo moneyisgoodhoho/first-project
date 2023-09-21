da116a258d7b0414db516df7a1755857ec10660b
siheyong Lee <tlgud119@naver.com>
2023-09-21 23:21:43 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const Burger = document.querySelector("Dream_burger");
+const menu = document.querySelector("Dream_menu");
+const sns = document.querySelector("Dream_SNS");
+Burger.onclick = openlist;
+
+function openlist(){
+ menu.style.display = "column"}
